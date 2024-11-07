cb59a313c4ded23482f2396b26d8fb529504110c
Wafi Faisal Falah <wafifaisal19@gmail.com>
2024-11-07 14:50:18 +0700

    Fixed Blog

--- blog-cms/src/types/blog.ts
+++ blog-cms/src/types/blog.ts
@@ -1,10 +1,12 @@
+import { Document } from "@contentful/rich-text-types";
+
 export interface IBlog {
   fields: {
     title: string;
     slug: string;
     category: string;
     date: string;
-    content: any;
+    content: Document;
     thumbnail: {
       fields: {
         file: {
